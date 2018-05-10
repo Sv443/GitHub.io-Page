cb6c7f8ba6732580dc43a1f8f001ef5b2e6881ba
Sv443 <38158426+Sv443@users.noreply.github.com>
2018-05-10 02:34:14 +0200

    website to show off my simple, minimalistic CSS

---
+++ simplecss.php
@@ -0,0 +1,70 @@
+<!--
+Code written by Hand by Sv443 / Sven Fehler
+-->
+
+<!DOCTYPE html>
+<html>
+<head>
+<link href="https://fonts.googleapis.com/css?family=Nunito|PT+Sans|Ubuntu" rel="stylesheet">
+
+<meta charset="utf-8"/>
+<meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1" />
+<meta name="author" content="Sv443">
+
+<!--basic css setup - is applied to all websites-->
+<link rel="stylesheet" type="text/css" href="/css/applytoall.css">
+<link rel="stylesheet" type="text/css" href="http://sv443.net/code/simplecss.css">
+
+<link rel="icon" type="image/vnd.microsoft.icon" href="/favicons/snet5.ico">
+<title>Sv443 - Simple CSS Styles</title>
+<style>
+#alignbtmr {
+	position: fixed;
+	bottom: 5px;
+	right: 5px;
+}
+
+#aligntl{
+	top: 5px;
+	left: 5px;
+}
+
+#aligntop{
+	top: 5px;
+	left: 20%;
+	text-align: left;
+}
+</style>
+</head>
+<body class="noul" link="blue" vlink="blue"><div style="font-family:'Nunito' sans-serif;font-size:20px;">
+
+<div align="left" class="noul" id="aligntl">
+<table id="tback"><tr><th style="width:10%;">
+<a id="backurl" href="https://sv443.github.io/code" target="_self"><figure id="fig04"><img src="/images/arrowleftmaterial_blue.png", width="100px", height="100px", alt="Sv443 Network - Work in Progress"/><figcaption><span id="lang6">Back</span></figcaption></figure></a>
+</th><th style="width:90%;">
+</th></tr></table></div>
+
+<div id="aligntop">
+<div style="font-size:35px;font-family:'Ubuntu', sans-serif;" align="center">Sv443 - Simple CSS Styles</div>
+
+<br><br>
+Add this stylesheet to your &lt;head&gt; element using this tag:<br>&lt;link rel="stylesheet" type="text/css" href="http://sv443.net/code/simplecss.css"&gt;<br>Or download and be able to edit it <a href="simplecss.css" download="simplecss.css">here</a>
+<br><br>
+
+<button id="greybtn">Simple Grey Button</button> - add with &lt;button id="greybtn"&gt;TEXT&lt;/button&gt;<br><br>
+<button id="roundgreybtn">Round Grey Button</button>&nbsp;&nbsp;<button id="roundgreybtn">^</button> - add with &lt;button id="roundgreybtn"&gt;TEXT&lt;/button&gt;<br><br>
+<button id="bluebtn">Simple Blue Button</button> - add with &lt;button id="bluebtn"&gt;TEXT&lt;/button&gt;<br><br>
+<button id="roundbluebtn">Round Blue Button</button>&nbsp;&nbsp;<button id="roundbluebtn">^</button> - add with &lt;button id="roundbluebtn"&gt;TEXT&lt;/button&gt;<br><br><br>
+
+<a href="#">&lt;a&gt;-tag without underline</a> - add with &lt;body class="noul"&gt;
+<br><br><br>
+Dark blue text selection (try it here)
+<br><br><br>
+
+<footer id="alignbtmr">
+&copy; <a href="/sv443" target="_blank">Sv443 / Sven Fehler</a> 2018 - Licensed under Creative Commons (Attribution)
+</footer>
+</div></div>
+<script src="/js/applytoall.js"></script>
+</body>
+</html>
