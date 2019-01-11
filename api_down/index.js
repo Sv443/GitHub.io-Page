9146a15dfef82445f444f25c81d77b194c7044b8
Sven Fehler <sven.fehler@web.de>
2019-01-11 11:46:17 +0100

    Add files via upload

--- api_down/index.js
+++ api_down/index.js
@@ -1,1 +1,17 @@
+const http = require("http");
+const reason = "My server broke and I need to reset it completely.";
 
+
+http.createServer((req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Request-Method', '*');
+    res.setHeader('Access-Control-Allow-Methods', allowedMethods.join(","));
+    res.setHeader('Access-Control-Allow-Headers', '*');
+
+
+    res.writeHead(503, {"Content-Type": "text/plain; utf-8"});
+    res.end("This API is down at the moment.\nReason: " + reason);
+
+}).listen(80, null, null, err => {
+    console.log("HTTP listener: " + err);
+});
